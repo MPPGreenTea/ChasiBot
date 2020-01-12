4d5d674f3c7f22683e5937d3ead2518ba167b022
MPP Green Tea <59781858+MPPGreenTea@users.noreply.github.com>
2020-01-12 11:53:44 -0600

    Update Permissions.js

--- src/Permissions.js
+++ src/Permissions.js
@@ -15,8 +15,65 @@
 (function (exports) {
 
   function Permissions() {
-    
+    this.data = {};
+    this.data.owners = [];
+    this.data.moderators = [];
+    this.data.bans = [];
   }
+
+  Permissions.prototype.set_level = function (id, level) {
+    switch (level) {
+      case -1:
+        this.data.bans.push(id);
+        this.data.owners.splice(this.data.owners.indexOf(id), 1);
+        this.data.moderators.splice(this.data.moderators.indexOf(id), 1);
+        break;
+      case 0:
+        this.data.owners.splice(this.data.owners.indexOf(id), 1);
+        this.data.moderators.splice(this.data.moderators.indexOf(id), 1);
+        this.data.bans.splice(this.data.bans.indexOf(id), 1);
+        break;
+      case 1:
+        this.data.moderators.push(id);
+        this.data.owners.splice(this.data.owners.indexOf(id), 1);
+        this.data.bans.splice(this.data.bans.indexOf(id), 1);
+        break;
+      case 2:
+        this.data.owners.push(id);
+        this.data.moderators.splice(this.data.moderators.indexOf(id), 1);
+        this.data.bans.splice(this.data.bans.indexOf(id), 1);
+        break;
+      default:
+        return false;
+    };
+
+    return true;
+  };
+
+  Permissions.prototype.get_level = function (id) {
+    if (this.data.owners.indexOf(id) >= 0)
+      return 2;
+
+    if (this.data.moderators.indexOf(id) >= 0)
+      return 1;
+
+    if (this.data.bans.indexOf(id) >= 0)
+      return -1;
+
+    return 0;
+  };
+
+  Permissions.prototype.load_data = function (settings) {
+    this.data.bans = settings.permissions_bans;
+    this.data.moderators = settings.permissions_moderators;
+    this.data.owners = settings.permissions_owners;
+  };
+
+  Permissions.prototype.save_data = function (settings) {
+    settings.permissions_bans = this.data.bans;
+    settings.permissions_moderators = this.data.moderators;
+    settings.permission_owners = this.data.owners;
+  };
 
   exports = Permissions;
 

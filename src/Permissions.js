55a218452f4a09b1f3ac79de06d61269388662d4
MPP Green Tea <59781858+MPPGreenTea@users.noreply.github.com>
2020-01-11 22:21:29 -0600

    Create Permissions.js

---
+++ src/Permissions.js
@@ -0,0 +1,23 @@
+/**
+ * Chasi Bot v1.2.0
+ * By ChaCha Si on 1/11/20
+ *
+ * Chasi Bot is a Multiplayer Piano Bot written for NodeJS. This
+ * bot provides commands not frequently seen in MPP bots created from
+ * 2012-2016. These commands include variables, permissions, and files.
+ *
+ * This is where the user permissions are set. This file works with the file
+ * CommandManager.js so that users could access commands that are at their
+ * access levels. This prevents regular users from accessing the owner commands
+ * without permission.
+ */
+
+(function (exports) {
+
+  function Permissions() {
+    
+  }
+
+  exports = Permissions;
+
+})(module.exports);

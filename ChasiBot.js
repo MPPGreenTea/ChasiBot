c4524d56538fcdcb6d74377d0485a5e036e4262d
MPP Green Tea <59781858+MPPGreenTea@users.noreply.github.com>
2020-01-11 20:35:40 -0600

    Create ChasiBot.js

---
+++ ChasiBot.js
@@ -0,0 +1,8 @@
+/**
+ * Chasi Bot v2.9.19
+ * By ChaCha Si on 1/11/20
+ *
+ * Chasi Bot is a Multiplayer Piano Bot written for NodeJS. This
+ * bot provides commands not frequently seen in MPP bots created from
+ * 2012-2016. These commands include variables, permissions, and files.
+ */
